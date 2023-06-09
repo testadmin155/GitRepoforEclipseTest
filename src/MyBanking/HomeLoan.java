e557eca0a8f135d019aa9072e417c80ea7c3a402
skane <skane@Muhammad>
2023-06-09 19:54:01 +0200

    Third Commit

--- src/MyBanking/HomeLoan.java
+++ src/MyBanking/HomeLoan.java
@@ -5,6 +5,7 @@ public class HomeLoan {
 	public static void main(String[] args) {
 		
 		System.out.println("This is a Smart HomeLoan");
+		System.out.println("This is a Special HomeLoan");
 	}
 
 }
