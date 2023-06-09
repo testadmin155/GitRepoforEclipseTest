d7e0507a8c7b1037c8dd4459a81f185598d57c9f
skane <skane@Muhammad>
2023-06-09 20:33:15 +0200

    Develope Branch commit

--- src/MyBanking/PersonalLoan.java
+++ src/MyBanking/PersonalLoan.java
@@ -4,6 +4,7 @@ public class PersonalLoan {
 
 	public static void main(String[] args) {
 		System.out.println("This is a Personal Loan");
+		System.out.println("This is a Smart Personal Loan");
 
 	}
 
